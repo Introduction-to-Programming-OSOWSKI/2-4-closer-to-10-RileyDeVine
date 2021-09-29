7046790213439c62a1046d8df3b49c43f099e1e2
rileydevine <824055@mystma.org>
2021-09-29 09:09:34 -0500

    submitted

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 #WRITE YOUR CODE IN THIS FILE
+
+#define close10
+def close10(x,y):
+    if abs(10-x)>abs(10-y):
+        return y
+
+    elif abs(10-y)>abs(10-x):
+        return x
+
+    else:
+        return 0
+
+print(close10(8, 9))
+
